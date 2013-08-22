1bd90bf7ad9164919401a3437790f387bcd13219
unknown <marcjoha@W001322.mtg.ad>
2013-08-22 16:37:00 +0200

    Init repo

---
+++ readme.md
@@ -0,0 +1,35 @@
+jQuery - Google Analytics Scolling Tracker
+=============
+
+This is a very simple tracker that helps you track if a web page visitor scrolls so an elemen is shown in screen. 
+
+Gettings started
+-------
+
+Gettings started is extremly simple, just add a reference to the jquery.ga-scrolltracker.js-file and start the tracking as so:
+	
+	$(document).ready(function() {
+		gaScrollTracker.track();
+	});
+	
+
+For each element that you want to track, just add a "data-dash" attributed called data-track - set the value to the uniqe label that you want to forward to Google Analytics.
+
+	<h2 data-track="subheader">Sub header</h2>
+	
+When the visitor scrolls so that this element is visible the script will send a tracking event to GA, like this:
+
+	_gaq.push(['_trackEvent', 'ScollTrack', '%%%data-tracker value goes here%%%%%%', document.location.pathname, '', true]);
+
+### Debugging
+
+If you want to try the script without calling Google Analytics all the time, just override the gaScrollTracker.trackElement()-menthod.
+
+	$(document).ready(function() {
+		// override for debugging.
+		gaScrollTracker.trackElement = function(strTest){
+			alert('Tracked: ' + strTest);
+		};
+		
+		gaScrollTracker.track();
+	});
---
+++ src/jquery.ga-scrolltracker.js
@@ -0,0 +1,58 @@
+
+(function($){
+   $.fn.inScreen = function() {
+	  var elem = $(this);
+	  var docViewTop = $(window).scrollTop();
+	  var docViewBottom = docViewTop + $(window).height();
+
+	  var elemTop = $(elem).offset().top;
+	  var elemBottom = elemTop + $(elem).height();
+
+	  var res = ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+	
+	  return res;
+   }; 
+})(jQuery);
+
+	
+var gaScrollTracker = {
+	callBackTime: 100,
+	arrTracked : [],
+	timer : 0,
+	track: function () {
+		
+		$(window).scroll(function() {
+			
+			if (gaScrollTracker.timer) {
+				clearTimeout(gaScrollTracker.timer);
+			}
+			
+			gaScrollTracker.timer = setTimeout(gaScrollTracker.runTracker, gaScrollTracker.callBackTime);
+				
+		});
+	},
+	runTracker : function(){
+		
+		// Get all elements with data-track-attributes and loop over them.
+		$("[data-track]").each(function(index) {
+		
+		  // is the element already tracked?
+		  if($.inArray(index, gaScrollTracker.arrTracked) == -1 && $(this).inScreen())
+		  {
+			// log
+			//console.log('Tracked: ' + $(this).data('track'));
+			
+			// add to array
+			gaScrollTracker.arrTracked.push(index);
+			gaScrollTracker.trackElement($(this).data('track'));
+			
+		  }
+		});
+	}, 
+	trackElement : function(strLabel){
+		var p = document.location.pathname;
+		if(typeof _gaq != 'undefined')
+			_gaq.push(['_trackEvent', 'ScollTrack', strLabel, p, '', true]);
+			
+	}
+}
